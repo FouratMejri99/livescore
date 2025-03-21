9fa6ce8cb98ea6c30377102d9c36f2d345e9abc2
Fourat Mejri <32815591+fouratMejri@users.noreply.github.com>
2025-03-21 18:15:27 +0100

    changes

--- src/app/api/news/route.ts
+++ src/app/api/news/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const response = await fetch(
-      `https://newsapi.org/v2/everything?q=football&from=2025-02-21&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`,
+      `https://newsapi.org/v2/everything?q=soccer&from=2025-02-21&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`,
       {
         headers: {
           "Content-Type": "application/json",
